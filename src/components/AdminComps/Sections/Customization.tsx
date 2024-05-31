ec1f7f08adcd94a608a8117892f18db8fddfc665
Nicolás Amicone <nicolas@pocimadigital.com>
2024-05-31 16:00:25 -0300

    modales

--- src/components/AdminComps/Sections/Customization.tsx
+++ src/components/AdminComps/Sections/Customization.tsx
@@ -1,9 +1,10 @@
 import React from "react"
 import { useConfig } from "../../../context/AdminContext"
+import EditTextModal from "../Buttons/EditTextModal";
 
 const data = [
-    { title: 'Titulo 1', description: 'CADA DETALLE IMPORTA EN TU IMAGEN', action: 'Editar' },
-    { title: 'Titulo 2', description: 'En nuestra peluquería, nos especializamos en ...', action: 'Editar' }
+    { title: 'Titulo 1', description: 'CADA **DETALLE** IMPORTA EN TU IMAGEN', action: 'Editar' },
+    { title: 'Titulo 2', description: 'En nuestra peluquería, nos especializamos en dar la mejor experiencia al cliente que nos elige garantizando su satisfaccion total en cada uno de nuestros servicios', action: 'Editar' }
 ];
 
 const Customization = () => {
@@ -14,11 +15,11 @@ const Customization = () => {
         <div className="mainContainer">
             <span className="initialTitle">¡Hola, <strong>{newConfig.customization.shopName}!</strong></span>
             <span className="proxApo">
-                Personalizar página
+                Personalizar textos
             </span>
             <div className="blackLayout">
                 <div className="proxApoHeader rowContainer">
-                    <div className="rowItem" style={{ width: "35%" }}><span>Secciones</span></div>
+                    <div className="rowItem" style={{ width: "35%" }}><span>Textos</span></div>
                     <div className="rowItem" style={{ width: "45%" }}><span>Vista Previa</span></div>
                     <div className="rowItem" style={{ width: "20%" }}><span>Editar</span></div>
                 </div>
@@ -26,9 +27,38 @@ const Customization = () => {
                     <div className="rowContainer" key={index}>
                         {Object.values(item).map((value, index) => (
                             <div className="rowItem" key={index} style={{ width: index === 0 ? '35%' : index === 1 ? '45%' : '20%' }}>
-                                <span>
-                                    {value}
-                                </span>
+                                {value !== "Editar" ? (
+                                    <span>{value}</span>
+                                ) : (
+                                    <div className="actionsContainer">
+                                        <EditTextModal initialTitle={item.description} />
+                                    </div>
+                                )}
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+            <span className="proxApo">
+                Personalizar imágenes
+            </span>
+            <div className="blackLayout">
+                <div className="proxApoHeader rowContainer">
+                    <div className="rowItem" style={{ width: "35%" }}><span>Imágenes</span></div>
+                    <div className="rowItem" style={{ width: "45%" }}><span>Vista Previa</span></div>
+                    <div className="rowItem" style={{ width: "20%" }}><span>Editar</span></div>
+                </div>
+                {data.map((item, index) => (
+                    <div className="rowContainer" key={index}>
+                        {Object.values(item).map((value, index) => (
+                            <div className="rowItem" key={index} style={{ width: index === 0 ? '35%' : index === 1 ? '45%' : '20%' }}>
+                                {value !== "Editar" ? (
+                                    <span>{value}</span>
+                                ) : (
+                                    <div className="actionsContainer">
+                                        <EditTextModal initialTitle={item.description} />
+                                    </div>
+                                )}
                             </div>
                         ))}
                     </div>
--- src/components/AdminComps/Sections/MainEditor.tsx
+++ src/components/AdminComps/Sections/MainEditor.tsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react"
 import { useConfig } from "../../../context/AdminContext"
 import { Link } from "react-router-dom";
-import EditModal from "../Buttons/EditModal";
+import EditTextModal from "../Buttons/EditTextModal";
 import DeleteModal from "../Buttons/DeleteModal";
-import { transform } from "typescript";
 
 let data = [
     { name: 'Nico Amico', date: '10/01', time: '13:00', action: 'Editar' },
@@ -14,6 +13,14 @@ let data = [
 
 data = data.concat(data).concat(data).concat(data).slice(0, 9)
 
+export const arrowIco = (inverted = 0) => {
+    return (
+        <svg style={{ transform: inverted ? `rotate(${inverted}deg)` : "none" }} width="30" height="17" viewBox="0 0 30 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16.4142 16.4142C15.6332 17.1953 14.3668 17.1953 13.5858 16.4142L0.857865 3.68629C0.076816 2.90524 0.0768161 1.63891 0.857865 0.857864C1.63891 0.076815 2.90524 0.0768151 3.68629 0.857864L15 12.1716L26.3137 0.857865C27.0948 0.0768161 28.3611 0.0768162 29.1421 0.857865C29.9232 1.63891 29.9232 2.90524 29.1421 3.68629L16.4142 16.4142ZM17 13L17 15L13 15L13 13L17 13Z" fill="white" fillOpacity="0.8" />
+        </svg>
+    )
+}
+
 const MainEditor = () => {
     const { config } = useConfig()
     const pageStep = 4
@@ -23,14 +30,6 @@ const MainEditor = () => {
     const handleSeeMore = () => {
         setVisibleItems(prevVisibleItems => prevVisibleItems + pageStep);
     };
-
-    const arrowIco = (inverted = false) => {
-        return (
-            <svg style={{ transform: inverted ? "rotate(180deg)" : "none" }} width="30" height="17" viewBox="0 0 30 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M16.4142 16.4142C15.6332 17.1953 14.3668 17.1953 13.5858 16.4142L0.857865 3.68629C0.076816 2.90524 0.0768161 1.63891 0.857865 0.857864C1.63891 0.076815 2.90524 0.0768151 3.68629 0.857864L15 12.1716L26.3137 0.857865C27.0948 0.0768161 28.3611 0.0768162 29.1421 0.857865C29.9232 1.63891 29.9232 2.90524 29.1421 3.68629L16.4142 16.4142ZM17 13L17 15L13 15L13 13L17 13Z" fill="white" fillOpacity="0.8" />
-            </svg>
-        )
-    }
 
     if (!config) return (<></>)
 
@@ -56,7 +55,6 @@ const MainEditor = () => {
                                 ) : (
                                     <div className="actionsContainer">
                                         <DeleteModal />
-                                        <EditModal />
                                     </div>
                                 )}
                             </div>
@@ -69,7 +67,7 @@ const MainEditor = () => {
                 {totalItems > visibleItems && (
                     <button className="seeAllButton" onClick={handleSeeMore}>
                         Ver más
-                        {arrowIco(false)}
+                        {arrowIco(0)}
                     </button>
                 )}
             </div>
