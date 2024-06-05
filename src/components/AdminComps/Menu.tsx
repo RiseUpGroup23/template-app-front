79d6327d595c08a03ef94879b2b21a17b5c0f34e
Nicolás Amicone <nicolas@pocimadigital.com>
2024-06-05 15:21:54 -0300

    admin mobile

--- src/components/AdminComps/Menu.tsx
+++ src/components/AdminComps/Menu.tsx
@@ -1,6 +1,7 @@
-import { useMediaQuery } from "@mui/material"
+import { Box, Drawer, List, ListItem, ListItemButton, ListItemText, useMediaQuery } from "@mui/material"
 import React, { useEffect, useState } from "react"
 import { Link, useLocation } from "react-router-dom";
+import MenuIcon from '@mui/icons-material/Menu';
 import "./AdminModules.css"
 
 const items = [
@@ -22,11 +23,16 @@ const Menu = () => {
     const isMobile = useMediaQuery('(max-width:1024px)');
     const [selected, setSelected] = useState(items.findIndex(item => item.url.includes(window.location.pathname)))
     const location = useLocation();
+    const [open, setOpen] = React.useState(false);
+
+    const toggleDrawer = (newOpen: boolean) => () => {
+        setOpen(newOpen);
+    };
 
     useEffect(() => {
         setSelected(items.findIndex(item => item.url.includes(window.location.pathname)))
     }, [location])
-    
+
     return (
         !isMobile ?
             <div className="menuContainer">
@@ -39,8 +45,24 @@ const Menu = () => {
                 )}
             </div>
             :
-            <>
-            </>
+            <div className="drawerButton">
+                <button onClick={toggleDrawer(true)}><MenuIcon /></button>
+                <Drawer open={open} onClose={toggleDrawer(false)}>
+                    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+                        <List>
+                            {items.map(({ title, url }, index) => (
+                                <Link to={url}>
+                                    <ListItem className={`menuItem ${selected === index ? "itemSelected" : ""}`} key={title} disablePadding>
+                                        <ListItemButton>
+                                            <ListItemText primary={title} />
+                                        </ListItemButton>
+                                    </ListItem>
+                                </Link>
+                            ))}
+                        </List>
+                    </Box>
+                </Drawer>
+            </div>
     )
 }
 
--- src/components/AdminComps/Sections/Appointments.tsx
+++ src/components/AdminComps/Sections/Appointments.tsx
@@ -8,7 +8,7 @@ const meses = [
 
 const diasSemana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
 
-function renderizarCalendario(mes: number, diaSeleccionado: number | null, onDiaClick: (dia: number) => void): JSX.Element[] {
+function renderizarCalendario(mes: number, diaSeleccionado: number | null, onDiaClick: (dia: number) => void, setDate: (date: Date) => void): JSX.Element[] {
     let año = new Date().getFullYear();
     if (mes > 12) {
         año += Math.floor((mes - 1) / 12); // Sumar años completos
@@ -36,7 +36,11 @@ function renderizarCalendario(mes: number, diaSeleccionado: number | null, onDia
         const isSelected = diaSeleccionado === i;
 
         calendarDays.push(
-            <div className={`calendarDay ${isSelected ? "selected" : ""}`} key={`current-${i}`} onClick={() => onDiaClick(i)}>
+            <div className={`calendarDay ${isSelected ? "selected" : ""}`} key={`current-${i}`} onClick={() => {
+                onDiaClick(i);
+                const selectedDate = new Date(año, mes - 1, i);
+                setDate(selectedDate);
+            }}>
                 <span>{i}</span>
             </div>
         );
@@ -58,22 +62,34 @@ function renderizarCalendario(mes: number, diaSeleccionado: number | null, onDia
 
 
 
+
 const Appointments = () => {
     const { newConfig } = useConfig()
     const [actualMonth, setActualMonth] = useState(new Date().getMonth())
     const [selected, setSelected] = useState(new Date().getDate())
+    const [date, setDate] = useState(new Date())
+
+    useEffect(() => {
+        if (actualMonth !== new Date().getMonth()) {
+            setSelected(0)
+        } else {
+            setSelected(new Date().getDate())
+            setDate(new Date())
+        }
+    }, [actualMonth])
+
     if (!newConfig) return (<></>)
     return (
         <div className="mainContainer">
             <span className="initialTitle">¡Hola, <strong>{newConfig.customization.shopName}!</strong></span>
             <span className="proxApo">
                 <div className="apoTitle">
-                    Todos los turnos - {meses[actualMonth]}
-                    <button onClick={() => setActualMonth(prev => prev - 1)}>
+                    Todos los turnos - {meses[actualMonth % 12]}
+                    {new Date().getMonth() < actualMonth && <button onClick={() => setActualMonth(prev => prev - 1)}>
                         <svg style={{ transform: "rotate(90deg)" }} width="30" height="17" viewBox="0 0 30 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M16.4142 16.4142C15.6332 17.1953 14.3668 17.1953 13.5858 16.4142L0.857865 3.68629C0.076816 2.90524 0.0768161 1.63891 0.857865 0.857864C1.63891 0.076815 2.90524 0.0768151 3.68629 0.857864L15 12.1716L26.3137 0.857865C27.0948 0.0768161 28.3611 0.0768162 29.1421 0.857865C29.9232 1.63891 29.9232 2.90524 29.1421 3.68629L16.4142 16.4142ZM17 13L17 15L13 15L13 13L17 13Z" fill="white" fillOpacity="0.8" />
                         </svg>
-                    </button>
+                    </button>}
                     <button onClick={() => setActualMonth(prev => prev + 1)}>
                         <svg style={{ transform: "rotate(270deg)" }} width="30" height="17" viewBox="0 0 30 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M16.4142 16.4142C15.6332 17.1953 14.3668 17.1953 13.5858 16.4142L0.857865 3.68629C0.076816 2.90524 0.0768161 1.63891 0.857865 0.857864C1.63891 0.076815 2.90524 0.0768151 3.68629 0.857864L15 12.1716L26.3137 0.857865C27.0948 0.0768161 28.3611 0.0768162 29.1421 0.857865C29.9232 1.63891 29.9232 2.90524 29.1421 3.68629L16.4142 16.4142ZM17 13L17 15L13 15L13 13L17 13Z" fill="white" fillOpacity="0.8" />
@@ -90,10 +106,16 @@ const Appointments = () => {
                         ))}
                     </div>
                     <div className="AdminCalendarContainer">
-                        {renderizarCalendario(actualMonth + 1, selected, setSelected)}
+                        {renderizarCalendario(actualMonth + 1, selected, setSelected, setDate)}
                     </div>
                 </div>
             </div>
+
+            {selected !== 0 &&
+                <span className="proxApo">
+                    {date.toLocaleDateString()}
+                </span>
+            }
         </div>
     )
 }
