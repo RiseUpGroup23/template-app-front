dc980f43bc84c15353c27373e596fe0d0cc552b0
Nicolás Amicone <nicolas@pocimadigital.com>
2024-07-15 15:18:52 -0300

    .

--- src/components/ComponentesTurnos/step0.tsx
+++ src/components/ComponentesTurnos/step0.tsx
@@ -2,13 +2,9 @@ import { useEffect, useMemo } from 'react';
 import ServiceCard from './Service/ServiceCard';
 import { useConfig } from '../../context/AdminContext';
 import CircularProgress from '@mui/material/CircularProgress';
-import { useStepContext } from '../../context/StepContext';
-import { useAppointment } from '../../context/ApContext';
 
 const Step0 = () => {
     const { config, services, fetchServices, professionals, fetchProfessionals } = useConfig()
-    const { nextStep, previous, prevStep } = useStepContext()
-    const { setForm } = useAppointment()
 
     const visibleCards = useMemo(() => {
         return services?.filter((service) => professionals?.some((prof) => prof.typesOfServices.some((e) => e._id === service._id))) ?? []
@@ -19,21 +15,6 @@ const Step0 = () => {
         !professionals?.length && fetchProfessionals()
         //eslint-disable-next-line
     }, [])
-
-    useEffect(() => {
-        if (visibleCards?.length === 1) {
-            if (previous < 1) {
-                setForm(prev => ({
-                    ...prev,
-                    typeOfService: visibleCards[0]._id
-                }))
-                nextStep(1)
-            } else {
-                prevStep(0)
-            }
-        }
-        //eslint-disable-next-line
-    }, [visibleCards])
 
     if (!config) return <></>
 
