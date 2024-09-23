78e55b06bb24ee3a4967451fe5b6418a9e193b5e
Nicolás Amicone <nicolas@pocimadigital.com>
2024-09-23 15:01:16 -0300

    barra admin home

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./header.css"
 import MenuTurnos from "./MenuTurnos";
 import MenuDrawer from "./MenuDrawer";
@@ -9,52 +9,72 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
 import FloatButtons from "./FloatButtons";
+import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
 
 const Header = () => {
-    const { config, invertColors } = useConfig()
+    const { config, invertColors, isAuthenticated } = useConfig()
     const isMobile = useMediaQuery('(max-width:1024px)');
     const location = useLocation()
     const [inverted, setInverted] = useState(localStorage?.getItem("inverted") ?? false)
     if (!config) return <></>
 
     return (
-        <div className="headerContainer" style={{ backgroundColor: `${hexToRgb(config.customization.primary.color, .75)}` }}>
-            <FloatButtons />
-            <Link to={"/"}>
-                <div className="logoHeader">
-                    <img src={config.customization.logo.primary} alt="logoImage" />
-                </div>
-            </Link>
+        <div className="headerContainer">
+            {isAuthenticated && <div className="modeAdminAd">
+                {!isMobile && <div className="modeAdminAd25"></div>}
+                <span className="modeAdminAd50">{isMobile ? "" : "Hola, BarberShop. "}
+                    Entraste en modo administrador
+                    <Tooltip title="En este modo podrás crear turnos salteando los pasos de pago si los tienes configurados en el sitio.">
+                        <HelpOutlineOutlinedIcon />
+                    </Tooltip>
+                </span>
+                <a className={isMobile ? "modeAdminAd50" : "modeAdminAd25"} href="/admin">
+                    Ir al <span style={{ textDecoration: "underline" }}>editor de página</span>
+                    <ArrowCircleRightOutlinedIcon />
+                </a>
 
-            {!isMobile ?
-                <div className="botonHeaderCont">
-                    {location.pathname === "/gestion-turnos" ? (
-                        <button onClick={() => window.location.href = `${window.location.origin}/admin`}>
-                            <button className="botonHeader">Admin</button>
-                        </button>
-                    ) : (
-                        <>
-                            <Link to={"/"}>
-                                <button style={{ color: `${config.customization.primary.text}` }} className={`botonHeader ${location.pathname === '/' ? 'active' : ''}`}>Inicio</button>
-                            </Link>
-                            <MenuTurnos />
-                            <button onClick={() => {
-                                if (localStorage.getItem("inverted")) {
-                                    localStorage.removeItem("inverted")
-                                } else {
-                                    localStorage.setItem("inverted", "true")
-                                }
-                                invertColors()
-                                setInverted((prev) => !prev)
-                            }} style={{ padding: `${config.customization.twoColors ? "" : "0"}`, visibility: `${config.customization.twoColors ? "visible" : "hidden"}`, color: `${config.customization.primary.text}` }} className={`botonHeader inverter`}>
-                                {inverted ? <LightModeIcon /> : <LightModeOutlinedIcon />}
+            </div>}
+            <div className="headerContent" style={{ backgroundColor: `${hexToRgb(config.customization.primary.color, .75)}` }}>
+                <FloatButtons />
+                <Link to={"/"}>
+                    <div className="logoHeader">
+                        <img src={config.customization.logo.primary} alt="logoImage" />
+                    </div>
+                </Link>
+
+                {!isMobile ?
+                    <div className="botonHeaderCont">
+                        {location.pathname === "/gestion-turnos" ? (
+                            <button onClick={() => window.location.href = `${window.location.origin}/admin`}>
+                                <button className="botonHeader">Admin</button>
                             </button>
-                        </>
-                    )}
-                </div>
-                :
-                <MenuDrawer />
-            }
+                        ) : (
+                            <>
+                                <Link to={"/"}>
+                                    <button style={{ color: `${config.customization.primary.text}` }} className={`botonHeader ${location.pathname === '/' ? 'active' : ''}`}>Inicio</button>
+                                </Link>
+                                <MenuTurnos />
+                                <button onClick={() => {
+                                    if (localStorage.getItem("inverted")) {
+                                        localStorage.removeItem("inverted")
+                                    } else {
+                                        localStorage.setItem("inverted", "true")
+                                    }
+                                    invertColors()
+                                    setInverted((prev) => !prev)
+                                }} style={{ padding: `${config.customization.twoColors ? "" : "0"}`, visibility: `${config.customization.twoColors ? "visible" : "hidden"}`, color: `${config.customization.primary.text}` }} className={`botonHeader inverter`}>
+                                    {inverted ? <LightModeIcon /> : <LightModeOutlinedIcon />}
+                                </button>
+                            </>
+                        )}
+                    </div>
+                    :
+                    <MenuDrawer />
+                }
+            </div>
         </div>
     )
 }
